1bf23b902deff4ed078a70385b08507e2875bf0e
Belvix <gokul.menon.2003@gmail.com>
2020-10-28 00:43:40 +0530

    changes

--- main.py
+++ main.py
@@ -11,26 +11,25 @@ while True:
     print(customer)
     start=input("Are you a customer or a mangager? Type c for customer and m for manager")
     restaurantlist = open("Restaurants/Restaurants.txt", 'r+')
-    if start=='c' or 'C' or 'customer' or 'Customer' and customer ==False and manager==False:
+    if start=='c' or 'C' or 'customer' or 'Customer':
         customer = True
         manager = False
         print("Welcome customer. Please take a look at the list of restaurants to choose from:-")
-        for restaurant in restaurantlist:
-            print(restaurant)
+        restaurants = restaurantlist.readlines()
+        restaurants = [line.rstrip('\n') for line in restaurants]
+        for i in restaurants:
+            print(i)
+            
             #restaurant = json.loads(restaurant)
             #print(restaurant['index']+':-'+restaurant['name'])
         
-    if start=='m' or 'M' or 'manager' or 'Manager' and customer ==False and manager==False:
+    elif start=='m' or 'M' or 'manager' or 'Manager':
         manager = True
         customer = False
-    if start=='exit':
+        
+    elif start=='exit':
         print("exiting")
         restaurantlist.close()
         break
-    if customer==True:
-        print("Welcome customer. Please take a look at the list of restaurants to choose from:-")
-        for restaurant in restaurantlist:
-            print(restaurant)
-            #restaurant = json.loads(restaurant)
-            #print(restaurant['index']+':-'+restaurant['name'])
+
         
