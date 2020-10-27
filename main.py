6ae9368f679bdb2fbd96685ba77cccbc153071e9
Belvix <gokul.menon.2003@gmail.com>
2020-10-28 00:52:44 +0530

    Fixed json

--- main.py
+++ main.py
@@ -6,27 +6,31 @@ import json
 
 customer = False
 manager = False
+restart = False
+
 
 while True:
     print(customer)
-    start=input("Are you a customer or a mangager? Type c for customer and m for manager")
+    start=input("Type C to order food, type M is you're a manager")
     restaurantlist = open("Restaurants/Restaurants.txt", 'r+')
-    if start=='c' or 'C' or 'customer' or 'Customer':
+    if start=='c' or start=='C' or start=='customer' or start=='Customer':
         customer = True
         manager = False
         print("Welcome customer. Please take a look at the list of restaurants to choose from:-")
         restaurants = restaurantlist.readlines()
         restaurants = [line.rstrip('\n') for line in restaurants]
-        for i in restaurants:
-            print(i)
-            
-            #restaurant = json.loads(restaurant)
-            #print(restaurant['index']+':-'+restaurant['name'])
+        print(restaurants)
+        for restaurant in restaurants:
+            restaurant = json.loads(restaurant)
+            print(restaurant['index']+':-'+restaurant['name'])
         
-    elif start=='m' or 'M' or 'manager' or 'Manager':
+    elif start=='m' or start=='M' or start=='manager' or start=='Manager':
         manager = True
         customer = False
-        
+    elif restart == True:
+        restart = False
+        customer = False
+        manager = False
     elif start=='exit':
         print("exiting")
         restaurantlist.close()
