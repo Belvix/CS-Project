e61d46155e7ec024a4b47faf7717beeb9ad5ed00
ZeroWing07 <csrichaitanya2003@gmail.com>
2020-10-31 19:45:17 +0530

    pull 3

--- manage.py
+++ manage.py
@@ -1,4 +1,10 @@
 #Shichi do  your thing here
 def manage():
-    global restaurantlist
-    print("Manage time!")
+    global Restaurants
+    restaurant_list = [line.rstrip('\n') for line in restaurants]
+    restaurant_dict_list = [json.loads(restaurant) for restaurant in restaurant_list]
+    for i in restaurant_dict_list:
+        print(i["name"])
+    
+
+
